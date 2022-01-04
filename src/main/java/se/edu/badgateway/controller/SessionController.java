a788b2870af871b4d55389ad9b5597390ebe5f27
Zhu <zjh815010623@gmail.com>
2022-01-04 21:01:13 +0800

    SimpleController

---
+++ src/main/java/se/edu/badgateway/controller/SessionController.java
@@ -0,0 +1,8 @@
+package se.edu.badgateway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SessionController {
+}
