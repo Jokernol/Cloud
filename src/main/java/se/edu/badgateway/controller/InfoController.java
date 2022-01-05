cab031fd33546c1d2701cad298952ddf09a0b5e2
Zhu <zjh815010623@gmail.com>
2022-01-05 17:09:18 +0800

    info

--- src/main/java/se/edu/badgateway/controller/InfoController.java
+++ src/main/java/se/edu/badgateway/controller/InfoController.java
@@ -2,6 +2,7 @@ package se.edu.badgateway.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,6 +24,22 @@ public class InfoController {
 
     @PostMapping("/add")
     public ModelAndView addInfo(InfoDTO infoDTO) {
-        return new ModelAndView();
+        infoService.addInfo(infoDTO);
+        return new ModelAndView("redirect:info");
     }
+
+    @PostMapping("/delete/{id}")
+    public ModelAndView deleteInfo(@PathVariable Integer id) {
+        infoService.deleteInfo(id);
+        return new ModelAndView("redirect:info");
+    }
+
+    @PostMapping("/get")
+    public ModelAndView getInfoList() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:info");
+        modelAndView.addObject("infoList", infoService.getInfoList());
+        return modelAndView;
+    }
+
 }
