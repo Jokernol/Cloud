9552e42933bb8dc37d357948063a9796d0d31d75
Zhu <zjh815010623@gmail.com>
2022-01-05 17:04:40 +0800

    info

--- src/main/java/se/edu/badgateway/pojo/DTO/InfoDTO.java
+++ src/main/java/se/edu/badgateway/pojo/DTO/InfoDTO.java
@@ -1,14 +1,9 @@
 package se.edu.badgateway.pojo.DTO;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDateTime;
 
 @Data
 public class InfoDTO {
     private String title;
     private String description;
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime time;
 }
--- src/main/java/se/edu/badgateway/service/InfoService.java
+++ src/main/java/se/edu/badgateway/service/InfoService.java
@@ -1,9 +1,35 @@
 package se.edu.badgateway.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.edu.badgateway.mapper.InfoMapper;
+import se.edu.badgateway.pojo.DO.Info;
+import se.edu.badgateway.pojo.DTO.InfoDTO;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
 public class InfoService {
+    @Resource
+    InfoMapper infoMapper;
+
+    public void addInfo(InfoDTO infoDTO) {
+        Info info = new Info();
+        info.setTime(LocalDateTime.now());
+        BeanUtils.copyProperties(infoDTO, info);
+
+        infoMapper.insert(info);
+    }
+
+    public void deleteInfo(Integer id) {
+        infoMapper.deleteById(id);
+    }
+
+    public List<Info> getInfoList() {
+        return infoMapper.selectList(null);
+    }
 }
